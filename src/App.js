b201df6e04bd905482955cf6c6ea8c445988a2e5
atude <mozamel.anwary1@gmail.com>
2019-05-05 20:05:09 +1000

    Setup appbar and sidbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import {blue, indigo, pink, red, } from '@material-ui/core/colors';
+import { AppBar, Typography, IconButton, Button, Toolbar, SwipeableDrawer, List, ListItem, ListItemText, Divider, ListItemIcon, } from '@material-ui/core';
+import { Menu, AccountBox, Buffer, } from 'mdi-material-ui';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: pink,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
+
+class App extends Component {  
+  constructor(props) {
+    super(props);    
+  }
+
+  state = {
+    left: false,
+  }
+
+  toggleDrawer = (side, open) => () => {
+    this.setState({
+      [side]: open,
+    });
+  };
+
+  getIcon = (index) => {
+    switch(index){
+      case 0: return <AccountBox/>;
+      case 1: return <Buffer/>;
+    }
+  }
+
+  render() {
+    const subjects = [
+      'About Me',
+      'My Projects'
+    ];
+
+    const sideList = (
+      <div className="Sidebar">
+        <List>
+          {subjects.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{this.getIcon(index)}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>       
+      </div>
+    );
+
+    return (
+      <div>
+      <MuiThemeProvider theme={theme}>
+     
+      <AppBar style={{zIndex: "100000"}} position="relative">
+        <Toolbar>
+          <IconButton onClick={this.toggleDrawer('left', !this.state.left)} color="inherit" aria-label="Menu">
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Mozamel Anwary
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <SwipeableDrawer open={this.state.left}
+      onClose={this.toggleDrawer('left', false)}
+      onOpen={this.toggleDrawer('left', true)}>
+        <div tabIndex={0} role="button"
+          onClose={this.toggleDrawer('left', false)} onKeyDown={this.toggleDrawer('left', false)}>
+          {sideList}
+        </div>
+      </SwipeableDrawer>
+
+      </MuiThemeProvider>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
