0bf98a86a2c18ed9bdc901da06520c938a4cee2f
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-08 03:28:15 +1100

    Add node to beams

--- src/components/ProjectMiniIcons.js
+++ src/components/ProjectMiniIcons.js
@@ -163,6 +163,15 @@ export const getMiniIcons = (types, setColor) => {
           </Tooltip>
         );
         break;
+      case "Node":
+        iconList.push(
+          <Tooltip disableFocusListener title={item}>
+            <SvgIcon fontSize="small" viewBox="0 0 600 600" style={miniIconColor}>
+              <g transform="translate(60, 60) scale(0.9, 0.95)"><path d="m485.291 129.408-224-128c-3.285-1.877-7.296-1.877-10.581 0l-224 128c-3.328 1.899-5.376 5.44-5.376 9.259v234.667c0 3.819 2.048 7.36 5.376 9.259l224 128c1.643.939 3.456 1.408 5.291 1.408s3.648-.469 5.291-1.408l224-128c3.328-1.899 5.376-5.44 5.376-9.259v-234.667c-.001-3.819-2.049-7.36-5.377-9.259z"/></g>
+            </SvgIcon>
+          </Tooltip>
+        );
+        break;
       case "Apollo":
         iconList.push(
           <Tooltip disableFocusListener title={item}>
