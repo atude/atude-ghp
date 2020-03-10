73a2a12f74c0fb6b4a76b81f58cd1b5d11f207e5
Mozamel Anwary <mozamel.anwary1@gmail.com>
2020-03-11 00:27:06 +1100

    add animation to about me profile pic

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../components/Components.css';
 import ContentCard from '../components/ContentCard.js';
 import { Grid, Avatar, Fade, Slide, Fab, Tooltip } from '@material-ui/core';
@@ -9,88 +9,93 @@ import ToolsContent from '../components/ToolsContent';
 
 import imgProfile from '../assets/profile.jpg';
 
-class AboutPage extends React.Component {
-  state = {
-    transitionDone: false,
-    isAvatarLoad: false,
+const AboutPage = (props) => {
+  const [transitionDone, setTransitionDone] = useState(false);
+  const [isAvatarLoad, setAvatarLoad] = useState(false);
+  const [yPos, setYpos] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    setYpos(window.scrollY);
   }
+
+  const { mainColor, currentScheme } = props;
+  const tBase = 700;
+  const tAdd = 300;
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <Fade in timeout={600}>
+      <div className="ParentCenterContainer">
+        <Grid container direction="row" spacing={24} alignItems="center" justify="center"
+        style={{overflow: transitionDone ? "inherit" : "hidden"}}>
+
+        <Grid item>
+          <Fade in={isAvatarLoad} timeout={700}>
+            <Avatar src={imgProfile} alt="profile"
+              style={{
+                width: 200, height: 200, margin: 20,
+              }} 
+              onLoad={() => setAvatarLoad(true)}
+            /> 
+          </Fade>
+          <div className="AvatarBgCircle" style={{marginLeft: 0.1 * yPos}}/>
+          <div className="AvatarBgCircle2" style={{marginTop: -200 - 0.1 * yPos}}/>
+          <div className="AvatarBgCircle3" style={{marginLeft: 40 - 0.1 * yPos}}/>
+        </Grid>
+
+        <Grid item lg={8} md={8} sm={10} xs={12}>
+          <Slide in timeout={tBase}>
+          <ContentCard mainColor={mainColor} currentScheme={currentScheme}
+            headingIcon={<HumanGreeting style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
+            heading="Hi, I'm Moz!" 
+            body={Database["About me"]}
+          />
+          </Slide>
+        </Grid>
   
-  setTransition = () => {
-    this.setState({transitionDone: true});
-  }
+        <Grid item lg={10} md={10} sm={10} xs={12}>
+          <Slide in timeout={tBase+tAdd*1} direction="up" onEntered={() => setTransitionDone(true)}>
+          <ContentCard mainColor={mainColor} currentScheme={currentScheme}
+            headingIcon={<CodeBraces style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
+            heading="Programming Experience" 
+            body={""}
+            content={<SkillsContent currentScheme={currentScheme}/>}
+          />
+          </Slide>
+        </Grid>
 
-  render() {    
-    const { mainColor, currentScheme } = this.props;
-    const tBase = 700;
-    const tAdd = 300;
-
-    return (
-      <Fade in timeout={600}>
-        <div className="ParentCenterContainer">
-          <Grid container direction="row" spacing={24} alignItems="center" justify="center"
-          style={{overflow: this.state.transitionDone ? "inherit" : "hidden"}}>
-
-          <Grid item>
-            <Fade in={this.state.isAvatarLoad} timeout={700}>
-              <Avatar src={imgProfile} alt="profile"
-                style={{
-                  width: 200, height: 200, margin: 20,
-
-                }} 
-                onLoad={() => (this.setState({isAvatarLoad: true}))}
-              /> 
-            </Fade>
-            <div className="AvatarBgCircle"/>
-            <div className="AvatarBgCircle2"/>
-            <div className="AvatarBgCircle3"/>
-          </Grid>
-
-          <Grid item lg={8} md={8} sm={10} xs={12}>
-            <Slide in timeout={tBase}>
-            <ContentCard mainColor={mainColor} currentScheme={currentScheme}
-              headingIcon={<HumanGreeting style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
-              heading="Hi, I'm Moz!" 
-              body={Database["About me"]}
-            />
-            </Slide>
-          </Grid>
-    
-          <Grid item lg={10} md={10} sm={10} xs={12}>
-            <Slide in timeout={tBase+tAdd*1} direction="up" onEntered={this.setTransition}>
-            <ContentCard mainColor={mainColor} currentScheme={currentScheme}
-              headingIcon={<CodeBraces style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
-              heading="Programming Experience" 
-              body={""}
-              content={<SkillsContent currentScheme={currentScheme}/>}
-            />
-            </Slide>
-          </Grid>
-
-          <Grid item lg={10} md={10} sm={10} xs={12}>
-            <Slide in timeout={tBase+tAdd*2} direction="up" onEntered={this.setTransition}>
-            <ContentCard mainColor={mainColor} currentScheme={currentScheme}
-              headingIcon={<CubeOutline style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
-              heading="Software Technologies" 
-              body={""}
-              content={<ToolsContent currentScheme={currentScheme}/>}
-            />
-            </Slide>
-          </Grid>
-          </Grid>
-        
-          <div className="DownloadFAB">
-            <Tooltip title="View/Download Resume" placement="left">
-              <Fab size="medium" component="a" href={Database["Resume"]} 
-              download="_resume_mozamel_anwary" aria-label="DownloadResume"
-              style={{color: "white", backgroundColor: mainColor, opacity: "0.8"}}>
-                <FileDownload/>
-              </Fab>
-            </Tooltip>
-          </div>
+        <Grid item lg={10} md={10} sm={10} xs={12}>
+          <Slide in timeout={tBase+tAdd*2} direction="up" onEntered={() => setTransitionDone(true)}>
+          <ContentCard mainColor={mainColor} currentScheme={currentScheme}
+            headingIcon={<CubeOutline style={{color: mainColor}} fontSize="large" className="ContentCardHeadIcon"/>}
+            heading="Software Technologies" 
+            body={""}
+            content={<ToolsContent currentScheme={currentScheme}/>}
+          />
+          </Slide>
+        </Grid>
+        </Grid>
+      
+        <div className="DownloadFAB">
+          <Tooltip title="View/Download Resume" placement="left">
+            <Fab size="medium" component="a" href={Database["Resume"]} 
+            download="_resume_mozamel_anwary" aria-label="DownloadResume"
+            style={{color: "white", backgroundColor: mainColor, opacity: "0.8"}}>
+              <FileDownload/>
+            </Fab>
+          </Tooltip>
         </div>
-      </Fade>
-    );
-  }
+      </div>
+    </Fade>
+  );
 }
 
 export default AboutPage;
